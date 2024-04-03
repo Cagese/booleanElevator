54234b796035b6a85b6716eda51a7c37a514b670
mikha <mikhail_morozov@mail.ru>
2024-04-03 23:13:24 +0300

    commit

--- const.py
+++ const.py
@@ -1,8 +1,11 @@
-debug = 0.25
-t_elev_movmement = 0.5 * debug
-t_elev_doors_movment = 1.5 * debug
-t_people_boarding = 2 * debug
-max_people_in_cabine = 10
+with open('const.txt') as file:
+    settings = list(map(lambda x:float(x.strip()),file.readlines()))
+
+debug = settings[0]
+t_elev_movmement = settings[1] * debug
+t_elev_doors_movment = settings[2] * debug
+t_people_boarding = settings[3] * debug
+max_people_in_cabine = settings[4]
 colors = ['#2C69A9','#6B6B6B','#C47D00','#690B6F']
 
 
